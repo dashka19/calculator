31671237ff397e6de780e856523c02f0cf2d0049
dashka19 <dasha22071998@mail.ru>
2017-10-09 21:30:26 +0300

    calculator

---
+++ calculator.h
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a =5;
+	int b =7;
+	cout << "a = ";
+	cin >> a; cout << a << endl;
+	cout << "b = ";
+	cin >> b; cout << b << endl;
+	int c = a + b;
+	cout << "Сумма = " << c << endl; 
+		int d =-18;
+		int e =17;
+		cout << "d = ";
+		cin >> d; cout << d << endl;
+		cout << "e = ";
+		cin >> e; cout << e << endl;
+		int f = d - e;
+		cout << "Разность = " << f << endl;
+			int g =56;
+			int h =17;
+			cout << "g = ";
+			cin >> g; cout << g << endl;
+			cout << "h = ";
+			cin >> h; cout << h << endl;
+			int j = g * h;
+			cout << "Умножение = " << j << endl;
+    return 0;
+}
